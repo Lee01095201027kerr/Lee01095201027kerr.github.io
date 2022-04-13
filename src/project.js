27a83c0fc03083dbdf1ff391369247fd40a55741
insight21 <2593377603@qq.com>
2022-04-13 23:12:57 +0800

    Improve difficulty

--- src/project.js
+++ src/project.js
@@ -1608,7 +1608,7 @@ window.__require = function e(t, n, o) {
           i.default.playerTouch && null != a.default.Instance.targetFruit && 1 == this.touchNum && (this.touchNum = 0, a.default.Instance.targetFruit.getComponent(cc.PhysicsCircleCollider).radius = a.default.Instance.targetFruit.height / 2, a.default.Instance.targetFruit.getComponent(cc.PhysicsCircleCollider).apply(), a.default.Instance.targetFruit.getComponent(cc.RigidBody).active = true,a.default.Instance.targetFruit.getComponent(cc.RigidBody).type = cc.RigidBodyType.Dynamic, a.default.Instance.targetFruit.getComponent(cc.RigidBody).linearVelocity = cc.v2(0, -800), a.default.Instance.targetFruit = null,
             // 生成指定水果
             this.scheduleOnce(function () {
-              /* if (setFruits) {
+              if (setFruits) {
                 let {startFruits} =  setFruits;
                 i.default.GameUpdateCtrl && (startFruits.length > t.createFruitCount ?
                   (a.default.Instance.createOneFruit(startFruits[t.createFruitCount]), t.createFruitCount++) :
@@ -1620,19 +1620,18 @@ window.__require = function e(t, n, o) {
                   t.createFruitCount++) : 3 == t.createFruitCount ? (a.default.Instance.createOneFruit(2),
                   t.createFruitCount++) : 4 == t.createFruitCount ? (a.default.Instance.createOneFruit(2),
                   t.createFruitCount++) : 5 == t.createFruitCount ? (a.default.Instance.createOneFruit(3),
-                  t.createFruitCount++) : t.createFruitCount > 5 &&
+                  t.createFruitCount++) : 
                   (a.default.Instance.createOneFruit(s.default.RandomInteger(0, 5)),
                     t.createFruitCount++))
-                  
-              } */
-              i.default.GameUpdateCtrl && (0 == t.createFruitCount ? (a.default.Instance.createOneFruit(0),
+              }
+              /* i.default.GameUpdateCtrl && (0 == t.createFruitCount ? (a.default.Instance.createOneFruit(0),
                   t.createFruitCount++) : 1 == t.createFruitCount ? (a.default.Instance.createOneFruit(1),
                   t.createFruitCount++) : 2 == t.createFruitCount ? (a.default.Instance.createOneFruit(2),
                   t.createFruitCount++) : 3 == t.createFruitCount ? (a.default.Instance.createOneFruit(3),
                   t.createFruitCount++) : 4 == t.createFruitCount ? (a.default.Instance.createOneFruit(4),
-                  t.createFruitCount++) : 
-                  (a.default.Instance.createOneFruit(s.default.RandomInteger(0, 7)),
-                    t.createFruitCount++))
+                  t.createFruitCount++) :
+                  (a.default.Instance.createOneFruit(s.default.RandomInteger(0, 4)),
+                    t.createFruitCount++)) */
             }, .5))
         }, t.prototype.closeTouch = function () {
           this.node.off(cc.Node.EventType.TOUCH_START, this.onTouchStart, this), this.node.off(cc.Node.EventType.TOUCH_MOVE, this.onTouchMove, this), this.node.off(cc.Node.EventType.TOUCH_END, this.onTouchEnd, this), this.node.off(cc.Node.EventType.TOUCH_CANCEL, this.onTouchEnd, this)
